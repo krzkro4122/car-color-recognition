500b843c1b794bfa38ed767c8f6040d34080a82d
krzkro4122 <krolkrzysztof1999@gmail.com>
2021-10-23 18:25:42 +0200

    working with DroidCam on Ubuntu and can get area of detected object

--- detect.py
+++ detect.py
@@ -1,20 +1,21 @@
 import cv2 as cv
 import numpy as np
 
+cap = cv.VideoCapture(1)
 whT = 320
 confThreshold = 0.5
 nmsThreshold = 0.2
 
 #### LOAD MODEL
-
 ## Coco Names
 classesFile = "coco.names"
 classNames = []
 with open(classesFile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
-print(classNames)
 
 ## Model Files
+# modelConfiguration = "yolov2-tiny.cfg"
+# modelWeights = "yolov2-tiny.weights"
 modelConfiguration = "yolov3-320.cfg"
 modelWeights = "yolov3-320.weights"
 net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
@@ -41,24 +42,33 @@ def findObjects(outputs,img):
     indices = cv.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
 
     for i in indices:
-        i = i[0]
         box = bbox[i]
         x, y, w, h = box[0], box[1], box[2], box[3]
+
+
+        # Here you can get the closer area where the detected item resides
+        # if limit == 0:
+        #     imagio = img[y:y + h, x:x + w]
+        #     cv.imshow('Frame', imagio)
+        #     cv.waitKey(1)
+
+
         cv.rectangle(img, (x, y), (x + w, y + h), (255, 0 , 255), 2)
         cv.putText(img,f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%',
                   (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
 
+limit = 0
 
-if __name__ == "__main__":
-    while True:
-        cap = cv.VideoCapture(0)
-        success, img = cap.read()
-        blob = cv.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
-        net.setInput(blob)
-        layersNames = net.getLayerNames()
-        outputNames = [(layersNames[i[0] - 1]) for i in net.getUnconnectedOutLayers()]
-        outputs = net.forward(outputNames)
-        findObjects(outputs,img)
+while True:
+    success, img = cap.read()
 
-        cv.imshow('Image', img)
-        cv.waitKey(1)
+    blob = cv.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+    net.setInput(blob)
+
+    layersNames = net.getLayerNames()
+    outputNames = [(layersNames[i - 1]) for i in net.getUnconnectedOutLayers()]
+    outputs = net.forward(outputNames)
+    findObjects(outputs, img)
+
+    cv.imshow('Image', img)
+    cv.waitKey(1)
