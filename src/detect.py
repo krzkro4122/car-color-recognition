a8e8b4206b9b1e0d02c8067b87979d977188e9af
Krzysztof Krol <krzysztof.krol@motorolasolutions.com>
2021-10-28 09:01:41 +0200

    File structure cleaned up for project readability.

--- src/detect.py
+++ src/detect.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
-import semantic_segmentation
+# import semantic_segmentation
 
 from copy import copy
 
 # --- Camera or demo video or demo picture --- #
 # cap = cv2.VideoCapture(0)
 # cap = cv2.VideoCapture(r"images/cars.mp4")
-img = cv2.imread(r"images/0197.jpg")
+img = cv2.imread(r"assets/0197.jpg")
 
 whT = 320
 confThreshold = 0.5
@@ -15,20 +15,20 @@ nmsThreshold = 0.2
 
 # --- LOAD MODEL --- #
 # Coco Names
-classesFile = "coco.names"
+classesFile = r"config/coco.names"
 classNames = []
 with open(classesFile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
 
 # Model Files
-# modelConfiguration = "yolov3-tiny.cfg"
-# modelWeights = "yolov3-tiny.weights"
-modelConfiguration = "yolov3-320.cfg"
-modelWeights = "yolov3-320.weights"
+# modelConfiguration = r"../config/yolov3-tiny.cfg"
+# modelWeights = r"../config/yolov3-tiny.weights"
+modelConfiguration = r"config/yolov3-320.cfg"
+modelWeights = r"config/yolov3.weights"
 
 net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-# net.setPreferableTarget(cv2.dnn.DDN_TARGET_CPU)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
 def findObjects(outputs, img):
     hT, wT, cT = img.shape
@@ -53,11 +53,11 @@ def findObjects(outputs, img):
         box = bbox[i]
         x, y, w, h = box[0], box[1], box[2], box[3]
 
-        return img[y:y+h, x:x+w] # Detect just 1 object
+        # return img[y:y + h, x:x + w] # Detect just 1 object
 
-        # cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0 , 255), 2)
-        # cv2.putText(img,f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%',
-        #           (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0 , 255), 2)
+        cv2.putText(img,f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%',
+                  (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
 
 
 # --- IMAGE --- #
