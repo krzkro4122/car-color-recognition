1ea1927ffb67cb16aefc05a567c25904a123d14b
Krzysztof Krol <krzysztof.krol@motorolasolutions.com>
2021-10-28 09:11:52 +0200

    Now only works with cars (simple workaround)

--- src/detect.py
+++ src/detect.py
@@ -5,9 +5,9 @@ import numpy as np
 from copy import copy
 
 # --- Camera or demo video or demo picture --- #
+img = cv2.imread(r"assets/0197.jpg")
 # cap = cv2.VideoCapture(0)
 # cap = cv2.VideoCapture(r"images/cars.mp4")
-img = cv2.imread(r"assets/0197.jpg")
 
 whT = 320
 confThreshold = 0.5
@@ -41,11 +41,12 @@ def findObjects(outputs, img):
             classId = np.argmax(scores)
             confidence = scores[classId]
             if confidence > confThreshold:
-                w, h = int(det[2] * wT) , int(det[3] * hT)
-                x, y = int((det[0] * wT) - w / 2) , int((det[1] * hT) - h / 2)
-                bbox.append([x, y, w, h])
-                classIds.append(classId)
-                confs.append(float(confidence))
+                if classNames[classId] == 'car':
+                    w, h = int(det[2] * wT) , int(det[3] * hT)
+                    x, y = int((det[0] * wT) - w / 2) , int((det[1] * hT) - h / 2)
+                    bbox.append([x, y, w, h])
+                    classIds.append(classId)
+                    confs.append(float(confidence))
 
     indices = cv2.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
 
@@ -53,12 +54,11 @@ def findObjects(outputs, img):
         box = bbox[i]
         x, y, w, h = box[0], box[1], box[2], box[3]
 
-        # return img[y:y + h, x:x + w] # Detect just 1 object
+        return img[y:y + h, x:x + w] # Detect just 1 object
 
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0 , 255), 2)
-        cv2.putText(img,f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%',
-                  (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
-
+        # cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0 , 255), 2)
+        # cv2.putText(img,f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%',
+        #           (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
 
 # --- IMAGE --- #
 blob = cv2.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
@@ -76,7 +76,7 @@ mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
 thresh, mask_black_and_white = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)
 masked_image = cv2.bitwise_or(img_after, img_after, mask=mask_black_and_white)
 
-cv2.imshow('Image before', img_before)
+cv2.imshow('Image before', img)
 cv2.imshow('Image after', img_after)
 cv2.imshow('Mask', mask)
 cv2.imshow('Masked image', masked_image)
