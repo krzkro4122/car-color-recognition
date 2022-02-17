0fdd939b8f535f52af069107fe85a24d19b46afa
Krzysztof Król <krkrol@student.agh.edu.pl>
2022-02-17 21:10:02 +0100

    Generated confusion matrices and better, working grid search

--- src/grid_search.py
+++ src/grid_search.py
@@ -1,26 +1,50 @@
 # Grid search
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.neural_network import MLPClassifier
+from preprocess_train_data import unpickle_data, feature_extraction
+import numpy as np
 
 parameter_space = {
-    'hidden_layer_sizes': [(50,50,50), (50,100,50), (100,), (100, 100, 100)],
+    'hidden_layer_sizes': [(100, 50, 25), (100, 70, 40, 10), (100, 45, 10), (70, 35, 17), (70, 52, 35, 17)],
+    'max_iter': [5000],
     'activation': ['tanh', 'relu'],
     'solver': ['sgd', 'adam'],
-    'alpha': [0.0001, 0.05, 1e-3, 5e-3, 0.5e-3],
+    'alpha': [0.0001, 0.05, 1e-3],
     'learning_rate': ['constant','adaptive'],
 }
 
-clf = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=3)
-clf.fit(X_train, y_train)
+pkl_name = "config/car_colors"
+width = 228
 
-print('Best parameters found:\n', clf.best_params_)
+data = unpickle_data(pkl_name, width)
+
+X = np.array(data["data"])
+y = np.array(data["label"])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.35,
+    shuffle=True,
+    random_state=42,
+)
+
+X_train, X_test = feature_extraction(X_train, X_test, data)
+
+mlp = MLPClassifier()
+
+mlp_from_grid = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=5, verbose=4)
+mlp_from_grid.fit(X_train, y_train)
+
+print('Best parameters found:\n', mlp_from_grid.best_params_)
 
 # All results
-means = clf.cv_results_['mean_test_score']
-stds = clf.cv_results_['std_test_score']
-for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+means = mlp_from_grid.cv_results_['mean_test_score']
+stds = mlp_from_grid.cv_results_['std_test_score']
+for mean, std, params in zip(means, stds, mlp_from_grid.cv_results_['params']):
     print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
 
-y_true, y_pred = y_test , clf.predict(X_test)
+y_true, y_pred = y_test , mlp_from_grid.predict(X_test)
 
 from sklearn.metrics import classification_report
 print('Results on the test set:')
